dda68925f9b9da44c9fd0e0c49aad91f180b7c7f
jazeem <jazeemmp404@gmail.com>
2024-06-15 21:31:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const tempFiled = document.getElementById('tempFiled');
+const toCelsius = document.getElementById('toCelsius');
+const toFahrenheit = document.getElementById('toFahrenheit');
+const result = document.getElementById('result');
+let temp;
+function convert(){
+    if(toFahrenheit.checked){
+       temp = Number(tempFiled.value);
+       temp = temp * 9 / 5 + 32;
+       result.textContent = temp.toFixed(2) + "°F"
+    }else if(toCelsius.checked){
+        temp = Number(tempFiled.value);
+        temp = (temp - 32 ) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C"
+    }else{
+        // result.textContent = "Select a unit"
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "bottom",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            }
+          });
+          Toast.fire({
+            icon: "error",
+            title: "Select a unit"
+          });
+    }
+}
